e9f57314177a47285fdf35f0fc2f16ee645c04d4
Denasia Furniss <106791069+djfurniss@users.noreply.github.com>
2022-07-19 16:27:04 -0400

    Update substitution.js
    Updating comments for submission;

--- src/substitution.js
+++ src/substitution.js
@@ -5,11 +5,11 @@
 
 const substitutionModule = (function () {
 
-  //---checks the uniqueness of a string's characters---
+  //---checks a string's uniqueness character by character---
   const checkUniqueness = (string)=>{ 
     charArr= string.split("") //each character = an element in an array
-    const result = charArr.reduce((acc, char)=>{ //reducing back into a string
-      if(!acc.includes(char)) acc += char //concats when the acc doesn't already include a previous character
+    const result = charArr.reduce((acc, char)=>{ //reducing back into a string one character at a time
+      if(!acc.includes(char)) acc += char //concats when the acc doesn't already include the same character
       return acc
     }, "")//reduce all unique characters into a separate string
   return result.length === string.length //returns a boolean; true if each character passes, false if not
@@ -18,15 +18,15 @@ const substitutionModule = (function () {
   function substitution(input, alphabet, encode = true) {
         //---guard clause(s)---
       if(!alphabet || alphabet.length !== 26 || !checkUniqueness(alphabet)) return false; 
-    input = input.toLowerCase(); //all lower cased
-    let result = "" //placeholder
+    input = input.toLowerCase(); //makes sure to handle input all lowercased
+    let result = "" //placeholder to return 
     let words = input.split(" ") //creates an array where each input word is an element 
-      words.forEach(word=>{
-        for (let i = 0; i < word.length; i++){ //loops through each character
+      words.forEach(word=>{ //executes on each word
+        for (let i = 0; i < word.length; i++){ //loops through each character of the word
             if(encode)result += alphabet[word[i].charCodeAt()-97] //offsets from charCodeAt 'a'
             if(!encode) result += String.fromCharCode(alphabet.indexOf(word[i])+97)
         }
-        result += " " //add a space after each word'
+        result += " " //keeps the spaces between each word;
       })
   return result.trim() //trims the extra space from the end; 
   }
