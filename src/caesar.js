9883b77c8917c8805c9a76117113d7e1c18ef716
Denasia Furniss <106791069+djfurniss@users.noreply.github.com>
2022-07-19 15:40:29 -0400

    Updated caesar.js
    Deleting extra comments to clean up for submission.

--- src/caesar.js
+++ src/caesar.js
@@ -30,41 +30,4 @@ return result.trim()
   };
 })();
 
-
-//------extra functionality testing-------
-// function caesar(input, shift, encode = true) {
-//   // if(!input || !shift || shift>25 || shift<-25) return false;
-//   let result = ''
-//   char = input.toLowerCase()
-  
-//     encode == false ? shift = shift*-1 : shift = shift;
-//   for (let i = 0; i < char.length; i++){
-//     if(char.charCodeAt(i)<97||char.charCodeAt>122){
-//         result += input[i];
-//     }else if(char.charCodeAt(i)+shift <97){
-//         result += String.fromCharCode(char.charCodeAt(i)+shift + 26)
-//     }else if(char.charCodeAt(i)+shift > 122){
-//         result += String.fromCharCode(char.charCodeAt(i)+shift-26)
-//     }else{
-//         result += String.fromCharCode(char.charCodeAt(i)+shift)
-//     }
-//   }//end of loop
-// console.log(result)
-// // return result 
-// }
-
-// caesar("I'm sad", -2) //encoding (g'k qyb)
-// caesar("g'k qyb", -2, false) //decoding (i'm sad)
-
-// // console.log('a,z'.charCodeAt(2)) //a = 97, z = 122
-// // console.log(122-97)//25
-// //String.fromCharCode(115)//> 's'
-//-------------------
-
-
-
-
-
-
-
 module.exports = { caesar: caesarModule.caesar };
