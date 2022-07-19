d278a4f5277a4ad64c0af18406dd5e3132c5c9f8
Denasia Furniss <106791069+djfurniss@users.noreply.github.com>
2022-07-19 15:56:38 -0400

    Update caesar.js
    Updating format and comments to explain code

--- src/caesar.js
+++ src/caesar.js
@@ -4,25 +4,26 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
   function caesar(input, shift, encode = true) {
-        if(!input ||!shift ||shift>25|| shift< -25) return false
-    let result = ''
-    input = input.toLowerCase()
-      encode == false ? shift = shift*-1 : shift = shift;
-  for (let i = 0; i < input.length; i++){
-    if(input.charCodeAt(i)<97||input.charCodeAt(i)>122){
-      result+= input[i];
-    }else if(input.charCodeAt(i)+shift <97){
-        result += String.fromCharCode(input.charCodeAt(i)+shift + 26)
-    }else if(input.charCodeAt(i)+shift > 122){
-        result += String.fromCharCode(input.charCodeAt(i)+shift-26)
-    }else{
-        result += String.fromCharCode(input.charCodeAt(i)+shift)
-    }
-  }//end of loop
-return result.trim()
+      //---guard clauses---
+    if(!input ||!shift ||shift>25|| shift<-25) return false;
+    
+    let result = '' //placeholder to return
+    input = input.toLowerCase() //input to lowercase
+    encode == false ? shift = shift*-1 : shift = shift; //determines which way to shift depending on the intention to encode or decode
+    
+    for (let i = 0; i < input.length; i++){ //loops through the input string to access each character
+      if(input.charCodeAt(i)<97||input.charCodeAt(i)>122){ //checks if the current index's character is not an alphabet
+        result+= input[i];
+      }else if(input.charCodeAt(i)+shift <97){ //makes sure the shift doesn't make the encoded message "fall off" the alphabet
+          result += String.fromCharCode(input.charCodeAt(i)+shift + 26)
+      }else if(input.charCodeAt(i)+shift > 122){ //makes sure the shift doesn't make the encoded message "fall off" the alphabet
+          result += String.fromCharCode(input.charCodeAt(i)+shift - 26)
+      }else{
+          result += String.fromCharCode(input.charCodeAt(i)+shift)
+      }
+    }//end of loop
+  return result.trim()
 }
 
   return {
